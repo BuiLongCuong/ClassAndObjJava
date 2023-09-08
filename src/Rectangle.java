e3526081e3f699afe3ebe5d2ce88bd4118a6bb52
BuiLongCuong <builongcuong76@gmail.com>
2023-09-08 11:36:42 +0700

    demo

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Rectangle {
     public double chieudai;
     public double chieurong;
@@ -14,15 +16,20 @@ public class Rectangle {
         double CV= (this.chieudai+this.chieurong)*2;
         System.out.println("Chu vi hình chữ nhật là : "+CV);
     }
-    public void showInfoHcn(){
-        System.out.println("Chiều dài của hình chữ nhật: "+ this.chieudai);
-        System.out.println("Chiều rộng của hình chữ nhật: "+ this.chieurong);
-    }
+//    public void showInfoHcn(){
+//        System.out.println("Chiều dài của hình chữ nhật: "+ this.chieudai);
+//        System.out.println("Chiều rộng của hình chữ nhật: "+ this.chieurong);
+//    }
 }
 class Main{
     public static void main(String[] args) {
-        Rectangle hcn1 = new Rectangle(5.6,9);
-        hcn1.showInfoHcn();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nhập chiều dài của hình chữ nhật: ");
+        double chieudai = input.nextDouble();
+        System.out.println("Nhập chiều rộng của hình chữ nhật: ");
+        double chieurong = input.nextDouble();
+        Rectangle hcn1 = new Rectangle(chieudai,chieurong);
+//        hcn1.showInfoHcn();
         hcn1.tinhDientich();
         hcn1.tinhChuvi();
     }
---
+++ src/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    public String name;
+    public int age;
+    public int[] arrScores;
+
+    Student(String name, int age){
+
+    }
+}
